e8c3e873e8fc8cf138f5c022f9f1a3e021cac16b
Abhinav <abhiasthana76@gmail.com>
2024-06-24 20:27:37 +0530

    Arrays in c++

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  // program about the arrays in c++
+  // int arr[10];
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  for (int i = 0; i <= 10; i++) {
+    std::cout << arr[i] << endl;
+  }
+  return 0;
+}
+

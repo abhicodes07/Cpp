12d33fa65d1d1acd963efb343839f0438bf4efc7
Abhinav <abhiasthana76@gmail.com>
2024-06-24 19:50:05 +0530

    Functions

--- prime.cpp
+++ prime.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
-
 using namespace std;
+
 int main () {
   int num;
-  std::cout << "Enter the number: " << endl;;
+  std::cout << "Enter the number: ";
   std::cin >> num;
-  if (num%2 == 0) {
-    std::cout << "The number is even." << endl;
+  int flag = 0;
+
+  if (num == 0 || num == 1) {
+  flag = 1;
+  }
+  
+  for (int i=2; i<num; i++) {
+    if (num % i == 0){
+      flag = 1;
+      break;
+    }
+  }
+  if (flag == 0) {
+    std::cout << "Number is prime." << endl;
   }
   else {
-  std::cout << "The number is odd." << endl;
+    std::cout << "Number is not prime." << endl; 
   }
-
   return 0;
 }
